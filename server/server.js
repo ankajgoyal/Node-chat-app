b841287a9e02f83b6138d6129583e31964b8d055
ankaj <ankajgoyal4@gmail.com>
2018-09-30 16:28:22 +0530

    emit to all users

--- server/server.js
+++ server/server.js
@@ -15,6 +15,29 @@ var io = socketIO(server);
 io.on("connection",(socket)=>{
     console.log("New User Connected");
 
+    // socket.emit("newEmail",{
+    //     from:"billgates@microsoft.com",
+    //     text:"My all wealth is yours"
+    // });
+
+    // socket.emit("newMessage",{
+    //     from:"Mamta",
+    //     text:"Whats up",
+    //     createdAt:"Today"
+    // })
+
+    socket.on("createMessage",(message)=>{
+        //console.log("Create message",message)
+        io.emit("new message bhadwe",{
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime
+        })
+    })
+    // socket.on("createEmail",(createEmail)=>{
+    //     console.log("create Email",createEmail)
+    // })
+
     socket.on("disconnect",()=>{
         console.log("User Disconnected");
     })
