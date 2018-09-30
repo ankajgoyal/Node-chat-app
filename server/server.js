ef0ee23415af7554843eda15bab83731b3194d71
ankaj <ankajgoyal4@gmail.com>
2018-09-30 11:24:34 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 
 var constPath  = path.join(__dirname,'../public');
+const port = process.env.PORT||3000
 
 //console.log(__dirname);
 //console.log(constPath);
@@ -10,8 +11,8 @@ const app = express();
 
 app.use(express.static(constPath))
 
-app.listen(3000,function(req,res,next){
-    console.log("Listening on port 3000")
+app.listen(port,function(req,res,next){
+    console.log(`Listening on port ${port}`)
 })
 
 
